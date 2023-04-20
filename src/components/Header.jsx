87afd7e32b8499f7d2b11a16630006b9c664d1eb
Miguel Barceló <miguel.barcelo@gmail.com>
2023-04-19 19:42:21 -0700

    feat: Add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 
 export default function Header() {
   return (
     <AppBar position='static' sx={{ marginBottom: '20px' }}>
       <Toolbar>
+        <LockOpenIcon sx={{ mr: 1 }} />
         <Typography variant='h6' component="div" sx={{ flexGrow: 1 }}>
           Decode App
         </Typography>
